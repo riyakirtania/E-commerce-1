0a34dffdf7a4e57938dcb8e70fabc3b2b7b8e5b6
Riya Kirtania <kirtania.riya10@gmail.com>
2023-07-01 16:52:57 +0530

    Added new product list in home page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,31 @@
-import './App.css';
-
+import "./App.css";
+import data from "./data";
 function App() {
   return (
     <div className="App">
       <header>
         <a href="/">Amazon</a>
-      </header> 
+      </header>
       <main>
-        List Products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => {
+            return (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.image} />
+                </a>
+                <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                  </a>
+                  <p><strong>${product.price}</strong></p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </main>
     </div>
   );
---
+++ my-app/src/data.js
@@ -0,0 +1,66 @@
+const data={
+    products:[
+        {
+            name:'Men Regular Fit Polo Shirt',
+            slug:'men-regular-fit-polo-shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:409,
+            countInStock:10,
+            brand:'Nike',
+            rating:3.9,
+            numReviews: 10,
+            description: ' Symbol is built on the pillars of quality, reliability and affordability to offer you lifestyle essentials that never go out of fashion. Never out of style, this Symbol classic polo features contrast inner placket as an accent detail. Styled from breathable pique cotton, this polo is cut in an easy regular fit with ribbed collar & sleeves to provide a better grip and high-low hem for extra comfort. Wear it alone or under a jacket to make this collar polo look great'
+        },
+        {
+            name:'Allen Solly Men Polo',
+            slug:'allen-solly-men-polo',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:599,
+            countInStock:10,
+            brand:'Allen Solly',
+            rating:4.1,
+            numReviews: 15,
+            description: 'Ace weekend dressing in this yellow solid Polo Neck T-shirt from Allen Solly by Allen Solly.'
+        },
+        {
+            name:'Van Heusen Polo Shirt',
+            slug:'van-heusen-men-regular-fit-polo-shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:599,
+            countInStock:15,
+            brand:'Van Heusen',
+            rating:4.3,
+            numReviews: 15,
+            description: 'Ace weekend dressing in this yellow solid Polo Neck T-shirt from Allen Solly by Allen Solly.'
+        },
+        {
+            name:'EYEBOGLER T Shirt for Men',
+            slug:'EYEBOGLER-Round-Neck-Full-Sleeve-Striped-T-Shirt-for-Men',
+            category:'T-Shirts',
+            image:'/images/p4.jpg',
+            price:459,
+            countInStock:13,
+            brand:'EYEBOGLER',
+            rating:4.5,
+            numReviews: 15,
+            description: 'This product is made of good quality material. Eyebogler bring you all good quality products. We provide different type of products with verities. Care instruction keep away from the items or products which may cause damage to this product.'
+        },
+        {
+            name:'U.S. POLO fit T-Shirt',
+            slug:'U.S.-POLO-ASSN.-Men-Printed-Regular-fit-T-Shirt',
+            category:'T-Shirts',
+            image:'/images/p5.jpg',
+            price:599,
+            countInStock:10,
+            brand:'U.S. POLO',
+            rating:3.0,
+            numReviews: 15,
+            description: 'Freshen up your loungewear rotation with this t-shirt from U.S. Polo Assn. Cut from soft and light-weight cotton jersey, it is finished with a crew neck and short sleeves. Keep the print in focus by teaming with solid separates.'
+        }
+    ]
+}
+
+export default data;
