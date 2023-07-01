7ebfc044f7c771074dc0b3ed884e4e8c3fe2cc9f
Riya Kirtania <kirtania.riya10@gmail.com>
2023-07-01 15:30:54 +0530

    E-Commerce 1

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Amazon</a>
+      </header> 
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
