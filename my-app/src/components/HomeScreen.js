2d1091df75b669310fb978887e8f0402b6a2ea77
Riya Kirtania <kirtania.riya10@gmail.com>
2023-07-12 18:55:45 +0530

    added homeScreen

--- my-app/src/components/HomeScreen.js
+++ my-app/src/components/HomeScreen.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import data from "../data";
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
 import {Link} from "react-router-dom";
 function HomeScreen() {
+  const [products,setProducts]=useState([]);
+  useEffect(()=>{
+    const fetchData=async()=>{
+      const result=await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  },[])
   return (
     <div>
       <h1>Featured Products</h1>
         <div className="products">
-          {data.products.map((product) => {
+          {products.map((product) => {
             return (
               <div className="product" key={product.slug}>
                 <Link to={`/product/${product.slug}`}>
